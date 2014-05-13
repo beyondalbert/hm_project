bfb9ceea94e913aaf6e651ff7bd3f97e124d0ed1
albert <albert@debian>
2014-05-12 22:21:17 -0700

    modified records api and test_data

--- app/api/records.rb
+++ app/api/records.rb
@@ -1,30 +1,48 @@
 class Records < Grape::API
-  format :json
-  resource :records do
-    get :hello do
-      {hello: "world"}
-    end
+	format :json
+	resource :records do
+		get :hello do
+			{hello: "world"}
+		end
 
 		desc "get device's data"
-    get do
-      Record.all
-    end
+		get do
+			@api_token = ApiToken.find_by_api_token(params[:api_token])
+			@user = @api_token.user if @api_token
+			unless @api_token
+				error!({code: 401, msg: "该操作没有权限！"})
+			end
 
-    desc "Create a record"
-    post do
-      @device = Device.where(serial_num: params[:serial_num]).first
-      Record.create!(device_id: @device.id, temperature: params[:temperature], pm25: params[:pm25], formaldehyde: params[:formaldehyde], detect_time: params[:detect_time].to_time)
+			@device = Device.find(params[:device_id])
+			unless @device && @device.user == @user
+				error!({code: 401, msg: "该操作没有权限！"})
+			end
 
-    end
+			case params[:time_slot]
+			when "yesterday"
+				@records = @device.yesterday_records
+			when "last_week"
+				@records = @device.time_slot_records("week")
+			when "last_month"
+				@records = @device.time_slot_records("month")
+			when "all"
+				@records = @device.time_slot_records("all")
+			else
+				@records = @device.today_records
+			end
 
-    desc "Return a record"
-    params do
-      requires :id, type: Integer, desc: "Record id"
-    end
-    route_param :id do
-      get do
-        Record.find(params[:id])
-      end
-    end
-  end
+			@records
+		end
+
+		desc "Create a record"
+		post do
+			@device = Device.where(serial_num: params[:serial_num]).first
+			@record = Record.new(device_id: @device.id, temperature: params[:temperature], pm25: params[:pm25], formaldehyde: params[:formaldehyde], detect_time: params[:detect_time].to_time)
+			if @record.save
+				@record
+			else
+				{code: 406, msg: "数据库存储失败！"}
+			end
+		end
+	end
 end
--- test_data.rb
+++ test_data.rb
@@ -1,10 +1,38 @@
 require 'typhoeus'
 require 'json'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+	adapter: "mysql2",
+	host: "localhost",
+	username: "root",
+	password: "albert",
+	database: "hm_dev"
+)
+
+class Record < ActiveRecord::Base
+end
+
+if !Record.first.nil?
+	Record.all.destroy_all
+end
+
+class Fixnum
+	SECONDS_IN_DAY = 24 * 60 * 60
+	def days
+		self * SECONDS_IN_DAY
+	end
+
+	def ago
+		Time.now - self
+	end
+end
 
 host = "http://localhost:3000"
+#host = "http://192.168.1.103"
 
 (1..30).each do |j|
-  (1..24).each do |i|
-    res_record = Typhoeus::Request.post(host + "/api/records", params: {serial_num: "1234567890", temperature: "#{rand(40)}", pm25: "#{rand(400)}", formaldehyde: "#{rand(40)}", detect_time: "2014-04-#{j} #{i}"})
-  end
+	(1..24).each do |i|
+		res_record = Typhoeus::Request.post(host + "/api/records", params: {serial_num: "1234567890", temperature: "#{rand(40)}", pm25: "#{rand(400)}", formaldehyde: "#{rand(40)}", detect_time: "#{(31-j).days.ago.strftime('%F')} #{i}"})
+	end
 end
